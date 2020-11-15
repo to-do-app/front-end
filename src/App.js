3d7b88facc86cc7cb15c67dc2e7902f361b4cab5
Álvaro García Fernández <alvarogarciafer@gmail.com>
2020-11-15 14:43:28 +0100

    fix test

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App" data-testid="app">
         <List
           list={this.state.list}
           doItem={this.doItem}
